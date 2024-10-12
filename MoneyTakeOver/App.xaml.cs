f9cbc2b23c0cc6cf559e8bb39ba418be048b9d2e
Leoncio200 <109309384+Leoncio200@users.noreply.github.com>
2024-10-11 20:20:25 -0600

    Agregar archivos de proyecto.

---
+++ MoneyTakeOver/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyTakeOver
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MoneyTakeOver/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyTakeOver
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
